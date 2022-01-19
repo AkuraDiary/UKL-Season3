717866e94724f572dda495486fa888a37b0034ea
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-19 19:09:31 +0700

    changed bg

--- index.php
+++ index.php
@@ -12,16 +12,53 @@ if(!isset($_SESSION['username'])){
 
 <html>
     <head>
-        <link rel="stylesheet" href="Styles/index.css">
-        <title>Aplikasi Pembayaran SPP</title>
+    <link rel="stylesheet" type="text/css" href="./Styles/index.css">  
+        <title>UKL Pembayaran SPP</title>
     </head>
 <body>
 <!-- Kita akan panggil menu navigasi -->
 <?php require_once("./misc/header.php"); ?>
-<h3>Selamat datang, <?= $username; ?></h3>
-            <br />
-Silahkan dikelola dengan baik yaa :) 
-            <hr />
+
+<div class="main">
+  <div id="wrapper">
+    <div class="content-holder">
+      <div class="hero-wrap fl-wrap full-height scroll-con-sec">
+        
+        <div class="impulse-wrap">
+          <div class="mm-par-wrap">
+            <div class="mm-parallax" data-tilt data-tilt-max="4" >
+              <div class="overlay"></div>
+              <div class="bg" data-bg="https://www.teahub.io/photos/full/340-3401662_adolf-hitler-quote-meme.jpg" style="background-image: url(https://www.teahub.io/photos/full/340-3401662_adolf-hitler-quote-meme.jpg);">
+                
+              </div>
+              
+              <div class="hero-wrap-item fl-wrap">
+                <div class="container"><div class="fl-wrap section-entry hiddec-anim" style="opacity: 1; ">
+                 <h1 class="BoardName">Selamat datang <?=$username; ?></h1>
+                  
+                  </div></div></div>
+              
+            </div>
+            
+          </div>
+        </div>
+        
+        <div class="hero-corner hiddec-anim" style="opacity: 1; transform: translate3d(0px, 0px, 0px);"></div>
+        
+        <div class="hero-corner2 hiddec-anim" style="opacity: 1; transform: translate3d(0px, 0px, 0px);"></div>
+        
+        <div class="hero-corner3 hiddec-anim" style="opacity: 1; transform: translate3d(0px, 0px, 0px);"></div>
+        
+        <div class="hero-corner4 hiddec-anim" style="opacity: 1; transform: translate3d(0px, 0px, 0px);"></div>
+        
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
 <?php require_once("./misc/footer.php"); ?>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script  src="./script.js"></script>
 </body>
 </html>
