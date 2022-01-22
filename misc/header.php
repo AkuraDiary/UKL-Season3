8ea1c9e584b2ed08be00f3b6d5c8df4f671d60ae
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-22 10:13:38 +0700

    changed header into dropdown

--- misc/header.php
+++ misc/header.php
@@ -6,7 +6,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="/mengukl/Styles/header.css">        
         
     </head>
@@ -18,7 +22,7 @@
     $hasil = mysqli_fetch_assoc($panggil);    
     ?>
     
-    <nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -31,24 +35,32 @@
       <li class="nav-item active">
         <a class="nav-link nav-link-active" href="/mengukl/index.php">Home</a>
       </li>
-      <li class="nav-item">        
-        <a class="nav-link" href="/mengukl/crud/siswa.php">Data Siswa</a>
+
+      <li class="nav-Item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLinkSis" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Siswa
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkSis">
+          <a class="dropdown-item" href="/mengukl/crud/siswa.php">Data Siswa</a>
+          <a class="dropdown-item" href="/mengukl/crud/kelas.php">Data Kelas</a>
+        </div>
       </li>
+
+      <li class="nav-Item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLinkSPP" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SPP
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkSPP">
+        <a class="dropdown-item" href="/mengukl/transaction/spp.php">Data SPP</a>
+        <a class="dropdown-item" href="/mengukl/transaction/transaksi.php">Transaksi</a>
+        <a class="dropdown-item" href="/mengukl/misc/history.php">History Pembayaran</a>
+        </div>
+      </li>
+
       <li class="nav-item">
         <a class="nav-link" href="/mengukl/crud/petugas.php">Data Petugas</a> 
       </li>
-      <li class="nav-item">
-      <a class="nav-link" href="/mengukl/crud/kelas.php">Data Kelas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/mengukl/transaction/spp.php">Data SPP</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/mengukl/transaction/transaksi.php">Transaksi</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/mengukl/misc/history.php">History Pembayaran</a>
-      </li>
+
       <li class="nav-item">
         <a class="nav-link logout" href="/mengukl/login-logout/logout.php">LogOut</a>
       </li>    
@@ -57,17 +69,23 @@
       <li class="nav-item active">
         <a class="nav-link nav-link-active" href="/mengukl/index.php">Home</a>
       </li>
+
       <li class="nav-item">
         <a class="nav-link" href="/mengukl/transaction/transaksi.php">Transaksi</a>
       </li>
+
       <li class="nav-item">
         <a class="nav-link" href="/mengukl/misc/history.php">History Pembayaran</a>
       </li>
+
       <li class="nav-item">
         <a class="nav-link logout" href="/mengukl/login-logout/logout.php">LogOut</a>
       </li>
-    </ul>
     <?php } ?>
+    </ul>
+    <span class="navbar-text">
+      <h3> <?php echo $hasil['username']; ?></h3>
+    </span>
   </div>
 </nav>
 </html>
